11390cb82a97e4c90267e6ba1c0d12ab58d9d8e7
chelseyqc <6721@holbertonstudents.com>
2023-12-19 15:21:39 +0800

    style: pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,16 +10,17 @@ def status():
     """returns a JSON response with status OK"""
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """returns the number of each object"""
     types = {'amenities': 'Amenity',
-                    'cities': 'City',
-                    'places': 'Place',
-                    'reviews': 'Review',
-                    'states': 'State',
-                    'users': 'User'
-                    }
+             'cities': 'City',
+             'places': 'Place',
+             'reviews': 'Review',
+             'states': 'State',
+             'users': 'User'
+             }
     stats = {}
     for key, type in types.items():
         stats[key] = storage.count(type)
