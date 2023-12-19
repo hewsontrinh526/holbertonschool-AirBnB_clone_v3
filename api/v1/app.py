e20966c82360fbc5138d1513eaa6ac66dd53da2b
hewsontrinh526 <6734@holbertonstudents.com>
2023-12-19 18:27:59 +1100

    Feat: Added 404 error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A file app.py that starts a Flask web application"""
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from os import getenv
 
@@ -14,6 +14,12 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def teardown(exception):
     """close storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """error 404"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
