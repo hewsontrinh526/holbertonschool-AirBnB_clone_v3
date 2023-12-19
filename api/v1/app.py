d203e4c5edf8e9ba2dbc92dd043fde0688ff825e
hewsontrinh526 <6734@holbertonstudents.com>
2023-12-19 17:53:05 +1100

    Fix: Ordered imports

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """A file app.py that starts a Flask web application"""
+from api.v1.views import app_views
 from flask import Flask
 from models import storage
-from api.v1.views import app_views
 from os import getenv
 
 
