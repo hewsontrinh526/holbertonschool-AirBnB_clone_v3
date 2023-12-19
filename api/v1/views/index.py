3cfb7c01df8be5977b214bb59cf4effe15d59417
chelseyqc <6721@holbertonstudents.com>
2023-12-19 15:17:43 +0800

    feat: created an endpoint for number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """The index file"""
 from flask import jsonify
+from models import storage
 from api.v1.views import app_views
 
 
@@ -8,3 +9,18 @@ from api.v1.views import app_views
 def status():
     """returns a JSON response with status OK"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """returns the number of each object"""
+    types = {'amenities': 'Amenity',
+                    'cities': 'City',
+                    'places': 'Places',
+                    'reviews': 'Review',
+                    'states': 'State',
+                    'users': 'User'
+                    }
+    stats = {}
+    for key, type in types.items():
+        stats[key] = storage.count(type)
+    return jsonify(stats)
