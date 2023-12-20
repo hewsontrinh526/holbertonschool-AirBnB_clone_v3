bde8f8b67edce888dc4d48fbd11d096fa3d107ac
hewsontrinh526 <6734@holbertonstudents.com>
2023-12-20 17:58:58 +1100

    Fix: Revert changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 """A file app.py that starts a Flask web application"""
-from models import storage
 from api.v1.views import app_views
-from flask import Blueprint
-from flask import Flask
-from flask import jsonify
-from flask import make_response
-from flask_cors import CORS
+from flask import Flask, jsonify, make_response, Blueprint
+from models import storage
 import os
-import threading
-
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
