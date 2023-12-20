67e4d07c208890d9e39887b29a75765c1501fa12
hewsontrinh526 <6734@holbertonstudents.com>
2023-12-20 19:09:29 +1100

    Feat: Added CORS implementation

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,13 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response, Blueprint
 from models import storage
+from flask_cors import CORS
 import os
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
